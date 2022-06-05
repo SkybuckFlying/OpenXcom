5a7bef4cc865227810ab8a222b67deb20de9e507
Skybuck Flying <skybuck2000@hotmail.com>
2022-06-05 15:13:58 +0200

    Scalars (SSE2/SIMD) removed.
    Note: related options in menu still exist and could be removed later.

--- src/Engine/Zoom.cpp
+++ src/Engine/Zoom.cpp
@@ -26,17 +26,6 @@
 
 #include "OpenGL.h"
 
-// Scale2X
-#include "Scalers/scalebit.h"
-
-// HQX
-
-#include "Scalers/hqx.h"
-
-// xBRZ
-
-#include "Scalers/xbrz.h"
-
 #if (_MSC_VER >= 1400) || (defined(__MINGW32__) && defined(__SSE2__))
 
 #ifndef __SSE2__
@@ -729,114 +718,6 @@ int Zoom::_zoomSurfaceY(SDL_Surface * src, SDL_Surface * dst, int flipx, int fli
 	int dgap;
 	static bool proclaimed = false;
 
-	if (Screen::use32bitScaler())
-	{
-		if (Options::useXBRZFilter)
-		{
-			// check the resolution to see which scale we need
-			for (size_t factor = 2; factor <= 6; factor++)
-			{
-				if (dst->w == src->w * (int)factor && dst->h == src->h * (int)factor)
-				{
-					xbrz::scale(factor, (uint32_t*)src->pixels, (uint32_t*)dst->pixels, src->w, src->h, xbrz::RGB);
-					return 0;
-				}
-			}
-		}
-
-		if (Options::useHQXFilter)
-		{
-			static bool initDone = false;
-
-			if (!initDone)
-			{
-				hqxInit();
-				initDone = true;
-			}
-
-			// HQX_API void HQX_CALLCONV hq2x_32_rb( uint32_t * src, uint32_t src_rowBytes, uint32_t * dest, uint32_t dest_rowBytes, int width, int height );
-
-			if (dst->w == src->w * 2 && dst->h == src->h * 2)
-			{
-				hq2x_32_rb((uint32_t*)src->pixels, src->pitch, (uint32_t*)dst->pixels, dst->pitch, src->w, src->h);
-				return 0;
-			}
-
-			if (dst->w == src->w * 3 && dst->h == src->h * 3)
-			{
-				hq3x_32_rb((uint32_t*)src->pixels, src->pitch, (uint32_t*)dst->pixels, dst->pitch, src->w, src->h);
-				return 0;
-			}
-
-			if (dst->w == src->w * 4 && dst->h == src->h * 4)
-			{
-				hq4x_32_rb((uint32_t*)src->pixels, src->pitch, (uint32_t*)dst->pixels, dst->pitch, src->w, src->h);
-				return 0;
-			}
-		}
-	}
-
-	if (Options::useScaleFilter)
-	{
-		// check the resolution to see which of scale2x, scale3x, etc. we need
-		for (size_t factor = 2; factor <= 4; factor++)
-		{
-			if (dst->w == src->w * (int)factor && dst->h == src->h * (int)factor && !scale_precondition(factor, src->format->BytesPerPixel, src->w, src->h))
-			{
-				scale(factor, dst->pixels, dst->pitch, src->pixels, src->pitch, src->format->BytesPerPixel, src->w, src->h);
-				return 0;
-			}
-		}
-	}
-
-	// if we're scaling by a factor of 2 or 4, try to use a more efficient function
-	/*
-	if (src->format->BytesPerPixel == 1 && dst->format->BytesPerPixel == 1)
-	{
-
-#ifdef __SSE2__
-		static bool _haveSSE2 = haveSSE2();
-
-		if (_haveSSE2 &&
-			!((ptrdiff_t)src->pixels % 16) &&
-			!((ptrdiff_t)dst->pixels % 16)) // alignment check
-		{
-			if (dst->w == src->w * 2 && dst->h == src->h * 2) return  zoomSurface2X_SSE2(src, dst);
-			else if (dst->w == src->w * 4 && dst->h == src->h * 4) return  zoomSurface4X_SSE2(src, dst);
-		} else
-		{
-			static bool complained = false;
-
-			if (!complained)
-			{
-				complained = true;
-				Log(LOG_ERROR) << "Misaligned surface buffers.";
-			}
-		}
-#endif
-
-// __WORDSIZE is defined on Linux, SIZE_MAX on Windows
-#if defined(__WORDSIZE) && (__WORDSIZE == 64) || defined(SIZE_MAX) && (SIZE_MAX > 0xFFFFFFFF)
-		if (dst->w == src->w * 2 && dst->h == src->h * 2) return  zoomSurface2X_64bit(src, dst);
-		else if (dst->w == src->w * 4 && dst->h == src->h * 4) return  zoomSurface4X_64bit(src, dst);
-#else
-		if (sizeof(void *) == 8)
-		{
-			if (dst->w == src->w * 2 && dst->h == src->h * 2) return  zoomSurface2X_64bit(src, dst);
-			else if (dst->w == src->w * 4 && dst->h == src->h * 4) return  zoomSurface4X_64bit(src, dst);
-		}
-		else
-		{
-			if (dst->w == src->w * 2 && dst->h == src->h * 2) return  zoomSurface2X_32bit(src, dst);
-			else if (dst->w == src->w * 4 && dst->h == src->h * 4) return  zoomSurface4X_32bit(src, dst);
-		}
-#endif
-
-		// maybe X is scaled by 2 or 4 but not Y?
-		if (dst->w == src->w * 4) return zoomSurface4X_XAxis_32bit(src, dst);
-		else if (dst->w == src->w * 2) return zoomSurface2X_XAxis_32bit(src, dst);
-	}
-	*/
 	if (!proclaimed)
 	{
 		Log(LOG_INFO) << "Using software scaling routine. For best results, try an OpenGL filter.";
